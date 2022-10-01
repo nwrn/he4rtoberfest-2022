6c7d5c8ea80a8c5ef067c5d89305e7b8359da0b1
gMatheus.dev <gmnp98@gmail.com>
2022-10-01 19:34:34 -0300

    Finalizando o desafio. Hello World Suado.

---
+++ He4rtober/src/heart/He4rtober.java
@@ -0,0 +1,7 @@
+package heart;
+
+public class He4rtober {
+    public static void main(String[] args) {
+        System.out.println("He4rtoberFest \n2022 O ano da virada, Hacktober daquele jeitão.\nVamo q vamo!");
+    }
+}
